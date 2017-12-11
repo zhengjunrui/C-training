20e7e344e61b286c1e556ffe9eb5649330cc032f
zjr <474511883@qq.com>
2017-12-11 08:19:24 +0800

    delete some files

--- cplus_training/1.cpp
+++ cplus_training/1.cpp
@@ -1,47 +1,17 @@
-#include<iostream>
-using namespace std;
-/*
-void swap(int *x,int *y)
-{
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
+#include<stdio.h>
 
-int main()
-{
-    void swap(int *x,int *y);
-    int a = 10;
-    int b = 30;
-    cout<<"the value of a is"<<a;
-    cout<<"\nthe value of b is"<<b;
-    swap(&a,&b);
-    cout<<"\nthe value of a is"<<a;
-    cout<<"\nthe value of b is"<<b; 
-    cout<<""<<endl;   
-    return 0;
-}
-*/
-
-void swap(int &x,int &y)
-{
-    int temp;
-    temp = x;
-    x = y;
-    y = temp;
-}
-
-int main()
-{
-
-    int a = 10;
-    int b = 30;
-    cout<<"the value of a is"<<a;
-    cout<<"\nthe value of b is"<<b;
-    swap(a,b);
-    cout<<"\nthe value of a is"<<a;
-    cout<<"\nthe value of b is"<<b; 
-    cout<<""<<endl;   
-    return 0;
+main(){
+	int a,b,c,i=0;
+	printf("不重复的三位数有\n");
+	for(a=1;a<=4;a++){
+		for(b=1;b<=4;b++){
+			for(c=1;c<=4;c++){
+				if(a!=b&&b!=c&&a!=c){
+					i++;
+					printf("%d%d%d ",a,b,c);
+				}
+			}
+		}
+	}
+	printf("\n总共有%d个不重复数字",i); 
 }
